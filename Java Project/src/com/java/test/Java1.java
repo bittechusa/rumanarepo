18a7368c0c53f3b01f3c3538c6f97666d9c39511
Rumana <rafroz000@gmail.com>
2014-12-03 11:49:17 -0600

    3rd

---
+++ Java Project/src/com/java/test/Java1.java
@@ -0,0 +1,27 @@
+package com.java.test;
+
+public class Java1 
+{
+	int a = 100;
+	int b = 200;
+	String c = "hello ";
+	String d = "Java";
+	void add()
+	{
+		System.out.println(a+b);
+		System.out.println(c+d);
+	}
+	void add(int x,int y)
+	{
+		System.out.println(x*y);
+	}
+	void add(String w, String q)
+	{
+		System.out.println("java how do u do? "+w+q);
+	}
+	void add(int k,String l)
+	{
+		System.out.println(k+l);
+	}
+
+}
---
+++ Java Project/src/com/java/test/Java2.java
@@ -0,0 +1,14 @@
+package com.java.test;
+
+public class Java2 
+{
+	public static void main(String[] args)
+	{
+		Java1 object = new Java1();
+		object.add();
+		object.add(40, 90);
+		object.add("habu", "dubu");
+		object.add(30, " age");
+	}
+
+}
